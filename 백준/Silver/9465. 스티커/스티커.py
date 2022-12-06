95b29b90c093e7bb52a59550f308ce76df0186b2
Cheol Hyeon <93192106+Risk-boy@users.noreply.github.com>
2022-12-06 09:29:19 +0900

    [Silver I] Title: 스티커, Time: 316 ms, Memory: 177408 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -15,13 +15,13 @@ for _ in range(T):
         dp1[1] = arr[0][1] + arr[1][0]
         dp2[1] = arr[0][0] + arr[1][1]
     for i in range(2, n):
-        dp1[i] = arr[0][i] + max(dp1[i-2], dp2[i-2], dp2[i-1])
-        dp2[i] = arr[1][i] + max(dp1[i-2], dp2[i-2], dp1[i-1])
+        dp1[i] = arr[0][i] + max(dp2[i-2], dp2[i-1])
+        dp2[i] = arr[1][i] + max(dp1[i-2], dp1[i-1])
     if n == 1:
         print(max(dp1[0], dp2[0]))
     else:
-        print(max(dp1[n-1], dp2[n-1], dp1[n-2], dp2[n-2]))
-    
+        print(max(dp1[n-1], dp2[n-1]))
+
 
 
 
