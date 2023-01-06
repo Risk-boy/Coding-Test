c4854f56548938c6eb160601ceb0ef75c6cefd96
Cheol Hyeon <93192106+Risk-boy@users.noreply.github.com>
2023-01-06 17:50:08 +0900

    [Silver I] Title: 숫자 재배치, Time: 728 ms, Memory: 31756 KB -BaekjoonHub

--- 백준/Silver/16943. 숫자 재배치/숫자 재배치.py
+++ 백준/Silver/16943. 숫자 재배치/숫자 재배치.py
@@ -1,43 +1,50 @@
 import sys
 # sys.stdin = open("input.txt")
+from copy import deepcopy
 
 '''
 A의 자리수가 B보다 더 크면 안됨
 A의 숫자들을 B의 자리수와 비교
 '''
-def solve(idx, number):
+def solve(x, idx, number):
     global answer
-    if number[0] == '0':
-        return
-    if len(number) == len(a): # 숫자를 다쓰고
-        if int(number) < int(b): # b보다 작으면!
-            if int(answer) < int(number):
-                answer = int(number)
+    if idx == 0 and x == 0:
         return
 
-    for j in range(len(a)):
-        if not visited[j]:
-            next = a[j]
-            visited[j] = True
-            solve(idx + 1, number + str(next))
-            visited[j] = False
+    if len(number) == len(a): # 숫자를 다쓰고
+        if int(number) < int(b): # b보다 작으면!
+            print(number)
+            exit()
+        else:
+            return
+
+    for j in range(len(temp)):
+        next = temp[j]
+        temp.remove(next)
+        temp.sort(reverse=True)
+        solve(next, idx + 1, number + str(next))
+        temp.append(next)
+        temp.sort(reverse=True)
     return
 
 A, b =  input().split()
 a = []
+
 for num in A:
     a.append(int(num))
-visited = [False for _ in range(len(a))]
-answer = -1
 
+a.sort(reverse=True)
 if len(a) > len(b):
     print(-1)
 else:
+    answer = ''
     for i in range(len(a)):
         start = a[i]
-        visited[i] = True
-        solve(0, str(start))
-        visited[i] = False
-    print(answer)
+        temp = deepcopy(a)
+        temp.remove(start)
+        temp.sort(reverse=True)
+        solve(a[i], 0, str(start))
+    else:
+        print(-1)
 
 
