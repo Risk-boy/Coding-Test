f47b3e5d1048764709d93119de1b5d8918da6a4b
Cheol Hyeon <93192106+Risk-boy@users.noreply.github.com>
2023-01-14 16:47:41 +0900

    [Gold V] Title: 줄어드는 수, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Gold/1174. 줄어드는 수/줄어드는 수.py
@@ -0,0 +1,24 @@
+import sys
+# sys.stdin = open("input.txt")
+
+def solve():
+    if ls:
+        result.add(int("".join(map(str, ls))))
+
+    for i in range(10):
+        if len(ls) == 0 or ls[-1] > i:
+            ls.append(i)
+            solve()
+            ls.pop()
+n = int(input())
+ls = []
+result = set()
+
+solve()
+
+result = list(result)
+result.sort()
+if n - 1 > len(result) - 1:
+    print(-1)
+else:
+    print(result[n - 1])
