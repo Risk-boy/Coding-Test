c6fb58789165e78b2170a801a723bf26f6cb1f91
Cheol Hyeon <93192106+Risk-boy@users.noreply.github.com>
2023-02-12 12:01:41 +0900

    [Bronze III] Title: 직각삼각형, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,12 @@
+import sys
+# sys.stdin = open("input.txt")
+
+while True:
+    nums = list(map(int, input().split()))
+    if nums[0] == 0:
+        break
+    nums.sort()
+    if nums[0] ** 2 + nums[1] ** 2 == nums[2] ** 2:
+        print("right")
+    else:
+        print("wrong")
