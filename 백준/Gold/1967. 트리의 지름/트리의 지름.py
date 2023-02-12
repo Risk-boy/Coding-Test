d8b3302357264da662427476ba2754e1ef26aa9b
Cheol Hyeon <93192106+Risk-boy@users.noreply.github.com>
2023-02-12 11:32:43 +0900

    [Gold IV] Title: 트리의 지름, Time: 184 ms, Memory: 247908 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -2,22 +2,18 @@ import sys
 # sys.stdin = open("input.txt")
 input = sys.stdin.readline
 sys.setrecursionlimit(int(1e5))
+
 '''
-리프노드들끼리만 연결
+임의의 한점으로부터 가장 먼 거리의 점을 찾고
+그 점으로 부터 가장 먼 곳에 있는 점과의 거리
+어떤분의 블로그를 참고했당
 '''
 
-def dfs(x, plus):
-    global max_d, distance
+def dfs(x, dist):
     for node in graph[x]:
-        if not visited[node[0]]:
-            distance += node[1]
-            visited[node[0]] = True
-            dfs(node[0], node[1])
-    if max_d < distance:
-        max_d = distance
-    visited[x] = False
-    distance -= plus
-    return
+        if distance[node[0]] == -1:
+            distance[node[0]] = dist + node[1]
+            dfs(node[0], dist + node[1])
 
 n = int(input())
 if n == 1:
@@ -29,12 +25,14 @@ for _ in range(n - 1):
     graph[p].append((c, d))
     graph[c].append((p, d))
 
+distance = [-1] * (n + 1)
+distance[1] = 0
+dfs(1, 0)
 
-visited = [False] * (n + 1)
-max_d = 0
-distance = 0
-for i in range(p + 1, n + 1):
-    visited[i] = True
-    dfs(i, 0)
+a = distance.index(max(distance))
+distance = [-1] * (n + 1)
+distance[a] = 0
+dfs(a, 0)
 
-print(max_d)
+print(max(distance))
+
